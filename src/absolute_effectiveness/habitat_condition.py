e22d969dbc810faa3d3d8b5ed3924dcf0b0753c6
Alana Lutz <81712531+alanalutz@users.noreply.github.com>
2026-04-02 13:49:06 -0600

    Improve intactness calculations with moving window (#2)
    * set up exponential kernel
    
    * adjust kernel parameters
    
    * convert test code to functions in HabitatConditionAnalyzer class
    
    * adjust parameters to prevent computation timeouts

--- src/absolute_effectiveness/habitat_condition.py
+++ src/absolute_effectiveness/habitat_condition.py
@@ -1,13 +1,17 @@
 import ee
-from math import sqrt
+import math
 from utils.variables import (
     ANALYSIS_END_YR,
     CRS,
-    MAX_EDGE_DIST,
-    MAX_PATCH_SIZE,
     MAX_PIXELS,
-    OPENING_RADIUS_EDGE,
     SCALE,
+    INTERACTION_DISTANCE,
+    BETA,
+    KERNEL_RADIUS_METERS,
+    INTACTNESS_SCALE,
+    KERNEL_RADIUS_PIXELS,
+    KERNEL_SIZE,
+    TILE_SCALE,
 )
 
 
@@ -17,20 +21,28 @@ class HabitatConditionAnalyzer:
     def __init__(
         self,
         analysis_end_yr=ANALYSIS_END_YR,
-        max_edge_dist=MAX_EDGE_DIST,
-        opening_radius_edge=OPENING_RADIUS_EDGE,
-        max_patch_size=MAX_PATCH_SIZE,
         crs=CRS,
         scale=SCALE,
         max_pixels=MAX_PIXELS,
+        interaction_distance=INTERACTION_DISTANCE,
+        beta=BETA,
+        kernel_radius_meters=KERNEL_RADIUS_METERS,
+        intactness_scale=INTACTNESS_SCALE,
+        kernel_radius_pixels=KERNEL_RADIUS_PIXELS,
+        kernel_size=KERNEL_SIZE,
+        tile_scale=TILE_SCALE,
     ):
         self.analysis_end_yr = analysis_end_yr
-        self.max_edge_dist = max_edge_dist
-        self.opening_radius_edge = opening_radius_edge
-        self.max_patch_size = max_patch_size
         self.crs = crs
         self.scale = scale
         self.max_pixels = max_pixels
+        self.interaction_distance = interaction_distance
+        self.beta = beta
+        self.kernel_radius_meters = kernel_radius_meters
+        self.intactness_scale = intactness_scale
+        self.kernel_radius_pixels = kernel_radius_pixels
+        self.kernel_size = kernel_size
+        self.tile_scale = tile_scale
 
     def get_habitat_raster(
         self, glc_processed, hgfc_processed, gpw_processed, nfw_processed
@@ -83,69 +95,69 @@ class HabitatConditionAnalyzer:
         )
         return min(habitat_area / site_area, 1)
 
-    def get_edge_distance_raster(self, habitat_raster, site_geom):
-        """Calculate distance from each habitat pixel to nearest non-habitat pixel."""
-        site_buffered = site_geom.buffer(self.max_edge_dist + self.opening_radius_edge)
-        projection = ee.Projection("EPSG:3857").atScale(30)
-        habitat_reprojected = habitat_raster.clip(site_buffered).reproject(projection)
-        habitat_binary = habitat_reprojected.unmask().eq(0)
-        habitat_binary_opened = habitat_binary.focalMin(
-            radius=self.opening_radius_edge, kernelType="square", units="meters"
-        ).focalMax(radius=self.opening_radius_edge, kernelType="square", units="meters")
-        edge_distance = habitat_binary_opened.distance(
-            ee.Kernel.euclidean(self.max_edge_dist, units="meters")
-        )
-        return (
-            edge_distance.min(self.max_edge_dist)
-            .unmask(self.max_edge_dist)
-            .clip(site_geom)
-            .selfMask()
-            .rename("edge_distance")
-        )
+    def build_kernel(self):
+        """Build an exponentially decaying distance-weighted kernel for calculating habitat intactness."""
+        # Create 2-D array of weights for kernel
+        weights = []
+        for row in range(self.kernel_size):
+            row_weights = []
+            for col in range(self.kernel_size):
+                dx = (
+                    col - self.kernel_radius_pixels
+                ) * self.intactness_scale  # horizontal distance from center pixel
+                dy = (
+                    row - self.kernel_radius_pixels
+                ) * self.intactness_scale  # vertical distance from center pixel
+                d = math.sqrt(
+                    dx * dx + dy * dy
+                )  # hypotenuse distance from center pixel
+                w = (
+                    math.exp(-self.beta * d) if d <= self.kernel_radius_meters else 0
+                )  # apply exponential decay and clip to kernel radius
+                row_weights.append(w)
+            weights.append(row_weights)
 
-    def calc_edge_distance_score(self, edge_distance_raster, site_geom):
-        """Calculate Edge Distance score from edge distance raster."""
-        avg_edge_distance = (
-            edge_distance_raster.reduceRegion(
+        # Build custom kernel with weights
+        exp_kernel = ee.Kernel.fixed(
+            width=self.kernel_size,
+            height=self.kernel_size,
+            weights=weights,
+            normalize=True,  # sum of all weights is 1; this normalizes the intactness values to be between 0 and 1
+        )
+        return exp_kernel
+
+    def get_intactness_raster(self, habitat_raster, site_geom, exp_kernel):
+        """Create continuous habitat intactness raster."""
+        # Get habitat binary
+        habitat_binary = habitat_raster.gt(0).unmask(0).toFloat()
+        habitat_binary_clipped = habitat_binary.clip(
+            site_geom.buffer(self.kernel_radius_meters)
+        )
+        # Apply kernel to habitat binary to get intactness raster
+        intactness_raster = (
+            habitat_binary_clipped.convolve(
+                exp_kernel
+            )  # sum the weighted habitat binary values in the kernel
+            .rename("intactness")
+            .updateMask(habitat_binary)
+        )  # only habitat pixels get intactness values
+        return intactness_raster
+
+    def calc_intactness_score(self, intactness_raster, site_geom):
+        """Calculate Habitat Intactness score within a PA from intactness raster."""
+        intactness_score = (
+            intactness_raster.reduceRegion(
                 ee.Reducer.mean(),
                 site_geom,
-                scale=self.scale,
+                scale=self.intactness_scale,
                 crs=self.crs,
                 maxPixels=self.max_pixels,
+                tileScale=self.tile_scale,
             )
-            .get("edge_distance")
+            .get("intactness")
             .getInfo()
         )
-        return avg_edge_distance / self.max_edge_dist
-
-    def get_patch_size_raster(self, habitat_raster, site_geom):
-        """Calculate size of connected habitat patches."""
-        return (
-            habitat_raster.gt(0)
-            .connectedPixelCount(maxSize=1024, eightConnected=False)
-            .rename("patch_size")
-            .clip(site_geom)
-        )
-
-    def calc_patch_size_score(self, patch_size_raster, site_geom):
-        """Calculate Patch Size score from patch size raster."""
-        patch_scale = sqrt((self.max_patch_size * 1000000) / 1024)
-        avg_patch_size = (
-            patch_size_raster.reduceRegion(
-                ee.Reducer.mean(),
-                site_geom,
-                scale=patch_scale,
-                crs=self.crs,
-                maxPixels=self.max_pixels,
-            )
-            .get("patch_size")
-            .getInfo()
-        )
-        return avg_patch_size / 1024
-
-    def calc_intactness_score(self, edge_distance_score, patch_size_score):
-        """Calculate Habitat Intactness score from edge distance and patch size."""
-        return sqrt(edge_distance_score * patch_size_score)
+        return intactness_score
 
     def calc_habitat_condition_score(
         self, habitat_extent_score, habitat_intactness_score
