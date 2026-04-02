518b3b668e7250e5b74d508e48f51f60cf6bf74c
Christian Thomas <christian@skytruth.org>
2026-04-02 12:54:52 -0400

    updated to output parquet, gave option to save intermediate files

--- src/psm/ps_grid_creation.py
+++ src/psm/ps_grid_creation.py
@@ -2,37 +2,21 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 from shapely.geometry import box
-
-
-# Infile
-infile = "/Users/christian/Desktop/TPA/data/TEST_PAS_4087.shp"
-gdf = gpd.read_file(infile)
-gdf = gdf.to_crs(epsg=4087)
-
-# Make Copies for Buffering, to maintain attributes
-gdf_10km_buff = gdf.to_crs(epsg=4087).copy()
-gdf_50km_buff = gdf.to_crs(epsg=4087).copy()
-
-gdf_10km_buff["geometry"] = gdf_10km_buff.buffer(distance=10000).to_crs(epsg=4087)
-gdf_50km_buff["geometry"] = gdf_50km_buff.buffer(distance=50000).to_crs(epsg=4087)
-# gdf_50km = gdf.buffer(distance=50000).to_crs(epsg=4087)
-
-# gdf_10km_buff.to_file("/Users/christian/Desktop/TPA/data/TEST_PAS_10km_4087.shp")
-# gdf_50km_buff.to_file("/Users/christian/Desktop/TPA/data/TEST_PAS_50km_4087.shp")
-ex_zone = gdf_10km_buff.copy()
-wider_landscape = gdf_50km_buff.copy()
-
-
-ex_zone["geometry"] = ex_zone.difference(gdf)
-wider_landscape["geometry"] = wider_landscape.difference(gdf_10km_buff)
-# ex_zone.to_file("/Users/christian/Desktop/TPA/data/exclusion_zone.shp")
-# wider_landscape.to_file("/Users/christian/Desktop/TPA/data/wider_landscape.shp")
-
+from utils.variables import (
+    WDPA_TEST_SITE_GEOJSON,
+    WDPA_TEST_SITE_10M_BUFFER,
+    WDPA_TEST_SITE_50M_BUFFER,
+    WDPA_EXCLUSION_ZONE,
+    WDPA_WIDER_LANDSCAPE,
+    WDPA_1KM_GRID,
+    WDPA_1KM_PSM_GRID,
+    PSM_CRS,
+)
 
 
 def make_grids(geom, crs, cell_size=1000, buffer_dist=5000):
     """
-    Create grids 
+    Create 1x1km grids
     """
     aoi = geom.buffer(buffer_dist)
     minx, miny, maxx, maxy = aoi.bounds
@@ -46,49 +30,108 @@ def make_grids(geom, crs, cell_size=1000, buffer_dist=5000):
     return grid[grid.intersects(aoi)].copy()
 
 
-poly = gdf_50km_buff.to_crs(epsg=4087)#.head(1)  # metric CRS
-grids = []
+def save_intermediate_gdf(gdf, output_path, save_intermediates):
+    if save_intermediates:
+        gdf.to_parquet(output_path)
 
-# Create the grids
-for idx, row in poly.iterrows():
-    grid = make_grids(
-        row.geometry,
-        crs=poly.crs,
-        cell_size=1000,   # 1 km cell sizes
-        buffer_dist=1000  # 1 km around each polygon
+
+def create_psm_cells(save_intermediates: bool=False):
+    # Read in test_site file, convert to 4087
+    pa_gdf = gpd.read_file(WDPA_TEST_SITE_GEOJSON)
+    pa_gdf = pa_gdf.to_crs(epsg=PSM_CRS)
+
+    # Make Copies for Buffering, to maintain attributes
+    pa_gdf_10km_buff = pa_gdf.to_crs(epsg=PSM_CRS).copy()
+    pa_gdf_50km_buff = pa_gdf.to_crs(epsg=PSM_CRS).copy()
+
+    # Run the Buffers
+    pa_gdf_10km_buff["geometry"] = pa_gdf_10km_buff.buffer(distance=10000)
+    pa_gdf_50km_buff["geometry"] = pa_gdf_50km_buff.buffer(distance=50000)
+
+    # Save intermediate files, if specified
+    save_intermediate_gdf(
+        pa_gdf_10km_buff,
+        WDPA_TEST_SITE_10M_BUFFER,
+        save_intermediates,
     )
-    grid["WDPA_PID"] = row["WDPA_PID"]   # keep WDPA_PID
-    grids.append(grid) 
+    save_intermediate_gdf(
+        pa_gdf_50km_buff,
+        WDPA_TEST_SITE_50M_BUFFER,
+        save_intermediates,
+    )
 
-grid_1km = gpd.GeoDataFrame(pd.concat(grids, ignore_index=True),crs=poly.crs)
-grid_1km = grid_1km.drop_duplicates(subset="geometry")
-# grid_1km.to_file("/Users/christian/Desktop/TPA/data/final_grid.geojson")
+
+    # Define the exclusion zone and wider landscapes
+    ex_zone = pa_gdf_10km_buff.copy()
+    wider_landscape = pa_gdf_50km_buff.copy()
+    ex_zone["geometry"] = ex_zone.difference(pa_gdf)
+    wider_landscape["geometry"] = wider_landscape.difference(pa_gdf_10km_buff)
+    
+    # Save intermediate files, if specified
+    save_intermediate_gdf(
+        ex_zone,
+        WDPA_EXCLUSION_ZONE,
+        save_intermediates,
+    )
+    save_intermediate_gdf(
+        wider_landscape,
+        WDPA_WIDER_LANDSCAPE,
+        save_intermediates,
+    )
+
+    poly = pa_gdf_50km_buff
+    grids = []
+
+    # Create the grids
+    for idx, row in poly.iterrows():
+        grid = make_grids(
+            row.geometry,
+            crs=poly.crs,
+            cell_size=1000,  # 1 km cell sizes
+            buffer_dist=1000,  # 1 km around each polygon
+        )
+        grid["WDPA_PID"] = row["WDPA_PID"]  # keep WDPA_PID
+        grids.append(grid)
+
+    grid_1km = gpd.GeoDataFrame(pd.concat(grids, ignore_index=True), crs=poly.crs)
+    grid_1km = grid_1km.drop_duplicates(subset="geometry")
+
+    # Save intermediate file, if specified
+    save_intermediate_gdf(
+        grid_1km,
+        WDPA_1KM_GRID,
+        save_intermediates,
+    )
+
+    # Aggregate geometries
+    pa_boundary = pa_gdf.union_all().boundary
+    ex_geom = ex_zone.union_all()
+    outside_geom = pa_gdf_50km_buff.union_all()
+    outside_boundary = outside_geom.boundary
+
+    # Stack exclusions
+    grid_1km["exclude"] = False
+    grid_1km["exclude"] |= grid_1km.intersects(pa_boundary)
+    grid_1km["exclude"] |= grid_1km.intersects(ex_geom)
+    grid_1km["exclude"] |= grid_1km.intersects(outside_boundary) | grid_1km.disjoint(
+        outside_geom
+    )
+
+    # Select the valid grid cells for the PA and it's wider landscape
+    in_out_grid_1km = grid_1km[grid_1km["exclude"] == False]
+
+    pa_union = pa_gdf.union_all()
+    in_out_grid_1km = in_out_grid_1km.copy()
+    in_out_grid_1km["site"] = np.where(
+        in_out_grid_1km.intersects(pa_union), 0, 1
+    ).astype("int8")
+
+    in_out_grid_1km["geometry"] = in_out_grid_1km.geometry.set_precision(
+        1.0
+    )  # 1 meter precision
+    in_out_grid_1km = in_out_grid_1km.drop("exclude", axis=1).to_crs(epsg=4326)
+    in_out_grid_1km.to_parquet(WDPA_1KM_PSM_GRID)
 
 
-# Aggregate geometries
-pa_boundary = gdf.union_all().boundary
-ex_geom = ex_zone.union_all()
-outside_geom = gdf_50km_buff.union_all()
-outside_boundary = outside_geom.boundary
-
-# Stack exclusions
-grid_1km["exclude"] = False
-grid_1km["exclude"] |= grid_1km.intersects(pa_boundary)
-grid_1km["exclude"] |= grid_1km.intersects(ex_geom)
-grid_1km["exclude"] |= (grid_1km.intersects(outside_boundary) | grid_1km.disjoint(outside_geom))
-
-# Select the valid grid cells for the PA and it's wider landscape 
-in_out_grid_1km = grid_1km[grid_1km["exclude"] == False]
-
-pa_union = gdf.union_all()
-in_out_grid_1km = in_out_grid_1km.copy()
-in_out_grid_1km["site"] = np.where(
-    in_out_grid_1km.intersects(pa_union),
-    0,
-    1
-).astype("int8")
-
-in_out_grid_1km["geometry"] = in_out_grid_1km.geometry.set_precision(1.0)  # 1 meter precision
-in_out_grid_1km = in_out_grid_1km.drop("exclude", axis=1)
-in_out_grid_1km.to_file("/Users/christian/Desktop/TPA/data/TPA_Valid_Grid.shp")
-
+if __name__ == "__main__":
+    create_psm_cells(save_intermediates=False)
