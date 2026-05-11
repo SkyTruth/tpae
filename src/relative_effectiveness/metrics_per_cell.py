2fc09e3652b5171b2bbc8086d9e08ee02da607b4
Alana Lutz <alanalutz01@gmail.com>
2026-05-11 13:17:29 -0600

    combine 2 relative effectiveness scripts into 1

--- src/relative_effectiveness/metrics_per_cell.py
+++ src/relative_effectiveness/metrics_per_cell.py
@@ -1,6 +1,56 @@
 import ee
 
 from absolute_effectiveness.habitat_condition import HabitatConditionAnalyzer
+from absolute_effectiveness.habitat_loss import HabitatLossAnalyzer
+
+__all__ = [
+    "RelativeHabitatLossAnalyzer",
+    "RelativeHabitatConditionAnalyzer",
+]
+
+
+class RelativeHabitatLossAnalyzer(HabitatLossAnalyzer):
+    """Per-cell habitat loss metric.
+
+    Reuses `get_habitat_loss_raster` from `HabitatLossAnalyzer` and scores
+    loss within each cell using a single reduceRegions pass.
+    """
+
+    def calc_loss_score_per_cell(self, habitat_loss_raster, habitat_start_raster, fc):
+        """Score Habitat Loss within each cell as `1 - (loss_area / start_area)`.
+
+        Cells with zero start-year habitat get a score of 0, mirroring the
+        absolute pipeline's guard in `calc_habitat_loss_score`.
+        """
+        loss_and_start = (
+            ee.Image.pixelArea()
+            .updateMask(habitat_loss_raster)
+            .rename("loss_area")
+            .addBands(
+                ee.Image.pixelArea()
+                .updateMask(habitat_start_raster)
+                .rename("start_area")
+            )
+        )
+
+        with_areas = loss_and_start.reduceRegions(
+            collection=fc,
+            reducer=ee.Reducer.sum(),
+            scale=self.scale,
+            crs=self.crs,
+        )
+
+        def add_score(feature):
+            habitat_loss_area = ee.Number(feature.get("loss_area"))
+            habitat_start_area = ee.Number(feature.get("start_area"))
+            if not habitat_start_area:
+                return feature.set("loss_score", 0)
+            habitat_loss_proportion = habitat_loss_area.divide(habitat_start_area).min(1)
+            return feature.set(
+                "loss_score", ee.Number(1).subtract(habitat_loss_proportion)
+            )
+
+        return with_areas.map(add_score)
 
 
 class RelativeHabitatConditionAnalyzer(HabitatConditionAnalyzer):
@@ -17,11 +67,13 @@ class RelativeHabitatConditionAnalyzer(HabitatConditionAnalyzer):
 
         # Could just set cell area to 1000000 since that's what it should be,
         # but this might be more accurate given the projection
-        
+
         cell_area_image = ee.Image.pixelArea().rename("cell_area")
-        habitat_area_image = cell_area_image.updateMask(habitat_raster).rename("habitat_area")
+        habitat_area_image = (
+            cell_area_image.updateMask(habitat_raster).rename("habitat_area")
+        )
         combined = habitat_area_image.addBands(cell_area_image)
-        
+
         with_area = combined.reduceRegions(
             collection=matched_grids,
             reducer=ee.Reducer.sum(),
@@ -58,3 +110,4 @@ class RelativeHabitatConditionAnalyzer(HabitatConditionAnalyzer):
             return feature.set("condition_score", extent.multiply(intactness))
 
         return fc.map(compute)
+
