3f33445b3307a1b68d32801fbc9055464b430bea
conordoremus <conor.doremus@gmail.com>
2026-05-12 12:49:05 -0700

    Added disturbance directory for python modules, added dist_variables to utils, modified DIST_ALERT ipynb

--- src/disturbance/DIST_STATUS.py
+++ src/disturbance/DIST_STATUS.py
@@ -2,12 +2,7 @@ import ee
 import geemap
 import pandas as pd
 
-FOLDERSET = {
-    '2025': "projects/glad/HLSDIST/DIST-ANN_v1_2025",
-    '2024': "projects/glad/HLSDIST/DIST-ANN_v1_2024",
-    '2023': "projects/glad/HLSDIST/DIST-ANN_v1",
-}
-
+from utils.dist_variables import(FOLDERSET)
 
 def calculate_site_disturbance_rate(year, test_site_id, test_sites):
     VEGDISTSTATUS = ee.ImageCollection(FOLDERSET[year] + "/VEG-DIST-STATUS").mosaic()
@@ -65,52 +60,3 @@ def calculate_all_sites_disturbance_rate(year, test_sites):
     df = pd.DataFrame([f['properties'] for f in features])
     df['year'] = year
     return df
-
-
-def visualize_site_disturbance(year, test_site_id, test_sites, Map=None):
-    VEGDISTSTATUS = ee.ImageCollection(FOLDERSET[year] + "/VEG-DIST-STATUS").mosaic()
-
-    dist_from = [0, 3, 6, 7, 8, 9, 10]
-    dist_to   = [0, 1, 2, 3, 4, 5, 6]
-    VEGDISTSTATUS_REMAP = VEGDISTSTATUS.remap(dist_from, dist_to, defaultValue=0)
-
-    site = test_sites.filter(ee.Filter.eq("SITE_ID", test_site_id))
-    site_name = site.first().get("NAME").getInfo()
-    site_geom = site.geometry()
-
-    site_DIST = VEGDISTSTATUS_REMAP.clip(site_geom)
-
-    palette = [
-        "121212",  # black
-        "E48727",  # light orange
-        "E01B07",  # red
-        "777777",  # grey
-        "DDDDDD",  # light grey
-        "005555",  # dark slate
-        "008888"   # dark cyan
-    ]
-
-    legend_dict = {
-        "No disturbance":          "#121212",
-        "Confirmed <50% ongoing":  "#E48727",
-        "Confirmed ≥50% ongoing":  "#E01B07",
-        "Confirmed <50% finished": "#777777",
-        "Confirmed ≥50% finished": "#DDDDDD",
-        "Prev year <50%":          "#005555",
-        "Prev year ≥50%":          "#008888",
-    }
-
-    if Map is None:
-        Map = geemap.Map()
-        Map.add_basemap("Esri.WorldImagery")
-        Map.addLayer(site_geom, {"color": "red"}, f"{site_name} Boundary")
-        Map.centerObject(site)
-        Map.add_legend(
-            title="DIST-ANN Disturbance Status",
-            legend_dict=legend_dict,
-            draggable=False,
-        )
-
-    Map.addLayer(site_DIST, {"min": 0, "max": 6, "palette": palette}, f"DIST {year} - {site_name}")
-
-    return Map
